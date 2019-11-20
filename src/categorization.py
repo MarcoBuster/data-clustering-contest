e34d36fa219c84b80a65d49951099be5151f7fea
Marco Aceti <mail@marcoaceti.it>
2019-11-21 00:18:51 +0100

    Add multiprocessing support for categorization

--- src/categorization.py
+++ src/categorization.py
@@ -1,5 +1,6 @@
 import glob
 import pickle
+import multiprocessing as mp
 
 import nltk
 
@@ -16,45 +17,53 @@ def _read_profile(lang, cat):
         return pickle.load(pf)
 
 
-def _calculate_distance(file, cat_profile):
-    with open(file, "r") as f:
-        file_ngrams = generate_ngrams(f.read())
-        nltk.jaccard_distance(cat_profile, file_ngrams)
+def _cat_file(file_path, cat_profiles):
+    with open(file_path, 'r') as f:
+        contents = parse_file(f.read())
+    lang = lang_detect(contents)
+    if lang not in ["en", "ru"]:
+        return None  # another language
+
+    ngrams = generate_ngrams(contents)
+    guesses = {c: 0 for c in CATEGORIES}
+    iter_cat = iter(CATEGORIES)
+    for category in cat_profiles[lang]:
+        guesses[next(iter_cat)] = nltk.jaccard_distance(ngrams, category)
+
+    min_value = min(guesses, key=guesses.get)
+    if guesses[min_value] > 0.97:
+        guess = "other"
+    else:
+        guess = min_value
+    return guess
 
 
 def categorize(path):
     cat_profiles = {"en": [], "ru": []}
-    result = {}
+    result = []
     for category in CATEGORIES:
         cat_profiles["en"].append(_read_profile("en", category))
         cat_profiles["ru"].append(_read_profile("ru", category))
-        result[category] = []
-    result["other"] = []
-
-    for file in glob.glob(path + '*.html'):
-        with open(file, 'r') as f:
-            contents = parse_file(f.read())
-        lang = lang_detect(contents)
-        if lang not in ["en", "ru"]:
-            continue
-
-        ngrams = generate_ngrams(contents)
-        guesses = {c: 0 for c in CATEGORIES}
-        iter_cat = iter(CATEGORIES)
-        for category in cat_profiles[lang]:
-            guesses[next(iter_cat)] = nltk.jaccard_distance(ngrams, category)
-
-        min_value = min(guesses, key=guesses.get)
-        if guesses[min_value] > 0.97:
-            guess = "other"
-        else:
-            guess = min_value
-        result[guess].append(file.split('/')[-1])
-
-    formatted_result = []
-    for cat in [*CATEGORIES, "other"]:
-        formatted_result.append({
-            "category": cat,
-            "articles": result[cat],
+        result.append({
+            "category": category,
+            "articles": []
         })
-    return formatted_result
+    result.append({
+        "category": "other",
+        "articles": []
+    })
+
+    files = glob.glob(path + "*.html")
+    futures = {}
+    pool = mp.Pool(processes=8)
+    for file in files:
+        futures[file.split('/')[-1]] = pool.apply_async(_cat_file, (file, cat_profiles))
+    pool.close()
+    pool.join()
+
+    for file in futures:
+        guess = futures[file].get()
+        if guess is None:
+            continue
+        result[result.index(next(i for i in result if i["category"] == guess))]["articles"].append(file)
+    return result
