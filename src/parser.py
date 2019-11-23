a9cdfaf896fc5a1500e951dfeb563f623aaaa1d6
Marco Aceti <mail@marcoaceti.it>
2019-11-23 16:38:26 +0100

    (disruptive commit) new and better generalized file parser

--- src/parser.py
+++ src/parser.py
@@ -1,4 +1,5 @@
 import lxml.html as html_parser
+from . import lang_detect
 
 
 def meta_tags(head):
@@ -7,31 +8,6 @@ def meta_tags(head):
         if not attrib.get('property') or not attrib.get('content'):
             continue
         yield (attrib.get('property').split(':')[1], attrib.get('content'))
-
-
-def parse_file(contents):
-    root = html_parser.fromstring(contents)
-    _childrens = root.getchildren()
-    head, body = _childrens[0], _childrens[1]
-    article = body.getchildren()[0]
-    """
-    for metadata in meta_tags(head):
-        print(metadata)
-
-    title = article.find("h1").text
-    _time = article.getchildren()[1].find("time")
-    if _time:
-        date_published = _time.attrib.get("datetime")
-    else:
-        date_published = "???"
-    """
-    body = article.text_content()
-    """
-    print('[TITLE]', title)
-    print('[DATE]', date_published)
-    print('[TEXT]', body)
-    """
-    return body
 
 
 def get_title_and_summary(contents):
@@ -58,3 +34,35 @@ def ranking_score(contents):
             links += hrefs
     figures = len(article.findall("figure"))
     return (words // 10) + (figures * 3) + (links * 1.5)
+
+
+def parse_file(filename):
+    with open(filename, 'r') as f:
+        raw_contents = f.read()
+    root = html_parser.fromstring(raw_contents)
+    _childrens = root.getchildren()
+    head, body = _childrens[0], _childrens[1]
+    article = body.getchildren()[0]
+    title = article.find("h1").text
+    """
+    for metadata in meta_tags(head):
+        print(metadata)
+
+    _time = article.getchildren()[1].find("time")
+    if _time:
+        date_published = _time.attrib.get("datetime")
+    else:
+        date_published = "???"
+    """
+    contents = article.text_content()
+    """
+    print('[TITLE]', title)
+    print('[DATE]', date_published)
+    print('[TEXT]', body)
+    """
+    return {
+        'filename': filename.split('/')[-1],
+        'title': title,
+        'contents': contents,
+        'lang': lang_detect.detect(contents),
+    }
