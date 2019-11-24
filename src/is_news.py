9cbd1ee902ce2951ceb46ed9fe3e1100262292e7
Marco Aceti <mail@marcoaceti.it>
2019-11-24 21:40:58 +0100

    add multiprocessing support for news command

--- src/is_news.py
+++ src/is_news.py
@@ -1,13 +1,21 @@
 import glob
+import multiprocessing as mp
 
 from . import parser
 
 
 def detect(path):
     files = glob.glob(path + '*.html')
-    result = {"articles": []}
+    pool = mp.Pool(processes=8)
+    futures = {}
     for file in files:
-        parsed_file = parser.parse_file(file, compute_news_score=True)
+        futures[file] = pool.apply_async(parser.parse_file, (file, ), {'compute_news_score': True})
+    pool.close()
+    pool.join()
+
+    result = {"articles": []}
+    for future in futures:
+        parsed_file = futures[future].get()
         if parsed_file["news_score"]:
             result["articles"].append(parsed_file["filename"])
     return result
