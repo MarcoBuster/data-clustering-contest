8a4306f2896818e369fc9a4a5b408ca71d0b3393
Marco Aceti <mail@marcoaceti.it>
2019-11-25 17:54:58 +0100

    json-ish error messages

--- start.py
+++ start.py
@@ -1,17 +1,24 @@
 import sys
 
 from src import main as src_main
+from src import common
 
 SUPPORTED_ACTIONS = ["languages", "news", "categories", "threads", "top"]
 
 
 def main():
     if len(sys.argv) < 3:
-        return print('Invalid number of arguments.')
+        return common.print_json({
+            'ok': False,
+            'error': 'Invalid number of arguments',
+        })
 
     action = sys.argv[1]
     if action not in SUPPORTED_ACTIONS:
-        return print('Invalid action.')
+        return common.print_json({
+            'ok': False,
+            'error': 'Invalid action selected',
+        })
 
     path = sys.argv[2]
     if not path.endswith('/'):
